1767bbb18367988f13e232712be2c12c11fad752
Raja Kadri <raja@rajas-mbp.lan>
2019-07-22 20:55:04 -0400

    Random used

--- test_script.py
+++ test_script.py
@@ -1,4 +1,8 @@
 #This program says hello and aks for a name.
+import random
+
+
+#code
 
 name = ''
 print('Hello World!')
@@ -17,12 +21,14 @@ print(len(myName))
 #	myList.append(c)
 #	if i > len(myName) - 1:
 #		break
-print('Select a number between 1 and ' + str(len(myName)) + ' or type skip to skip this.' )
+print('Select a number between 1 and ' + str(len(myName)) + ' or type skip or type random.' )
 
 #Can make input an int using int(), but will fix later
 
 myChar = input().lower()
-while myChar != 'skip':
+
+#Not skip or random
+while myChar != 'skip' and myChar != 'random':
 	if int(myChar) >= 1:
 		if  int(myChar) <= len(myName):
 			myList = []
@@ -34,6 +40,14 @@ while myChar != 'skip':
 	else:
 		print('Please enter a valid number.')
 		myChar = input().lower()
+
+#random
+if myChar == 'random':
+	myRandomList = []
+	for i, c in enumerate(myName):
+		myRandomList.append(c)
+	print(*myRandomList[random.randint(0, len(myName)-1)])
+
 print('What is your age?')
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
