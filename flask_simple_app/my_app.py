3364778b5959d479653a6d5478dc16498485e9df
Raja Kadri <raja@rajas-mbp.lan>
2019-07-28 18:10:35 -0400

    created html template for flask

--- flask_simple_app/my_app.py
+++ flask_simple_app/my_app.py
@@ -1,10 +1,13 @@
 #new flask app
 
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-	return "Hello World!"
+@app.route('/hello/<name>')
+def hello(name=None):
+	return render_template('hello.html', name=name)
 
+if __name__ == '__main__':
+	app.run()
